893a3337949c7dc8c76bd2efcd620803bd5b6179
kiboocodes <mihajlopopovic049@gmail.com>
2024-11-06 22:48:27 +0100

    Added input form component

---
+++ react-domaci-chatgpt/src/components/InputForm.jsx
@@ -0,0 +1,36 @@
+import React, { useState } from "react";
+
+function InputForm({ onSubmit }) {
+	const [input, setInput] = useState("");
+
+	const handleChange = (e) => setInput(e.target.value);
+
+	const handleSubmit = (e) => {
+		e.preventDefault();
+		if (input.trim()) {
+			onSubmit(input);
+			setInput("");
+		} else {
+			alert("Please enter a message");
+		}
+	};
+
+	return (
+		<form onSubmit={handleSubmit} className="mt-3">
+			<div className="input-group">
+				<input
+					type="text"
+					className="form-control"
+					placeholder="Type a message"
+					value={input}
+					onChange={handleChange}
+				/>
+				<button className="btn btn-primary" type="submit">
+					Send
+				</button>
+			</div>
+		</form>
+	);
+}
+
+export default InputForm;
