d10a25d98d0de1951bf2caca70276406e0d50368
kiboocodes <mihajlopopovic049@gmail.com>
2024-11-06 22:48:06 +0100

    Made footer component

---
+++ react-domaci-chatgpt/src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Footer() {
+	return (
+		<footer className="bg-dark text-white text-center py-3 mt-5">
+			<p>&copy; 2024 ChatGPT App. All rights reserved.</p>
+		</footer>
+	);
+}
+
+export default Footer;
