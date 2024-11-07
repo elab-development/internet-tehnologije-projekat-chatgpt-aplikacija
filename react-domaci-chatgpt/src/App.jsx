ad271a1a94c1c3a52d49140271925c8545270512
mihajlopop01 <popovic.mihajlo1@gmail.com>
2024-11-07 18:52:38 +0100

    Sredio rutiranje u app.jsx da radi kako treba

--- react-domaci-chatgpt/src/App.jsx
+++ react-domaci-chatgpt/src/App.jsx
@@ -1,7 +1,46 @@
-import React from "react";
+import React, { useState } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import ChatPage from "./pages/ChatPage";
+import AboutPage from "./pages/AboutPage";
+import LoginPage from "./pages/LoginPage";
+import Navbar from "./components/Navbar";
+import ProtectedRoute from "./components/ProtectedRoute";
+import Footer from "./components/Footer";
+import "./index.css";
 
 function App() {
-	return <div>App</div>;
+	const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+	return (
+		<Router>
+			<div className="d-flex flex-column min-vh-100">
+				<Navbar isLoggedIn={isLoggedIn} setIsLoggedIn={setIsLoggedIn} />
+				<div className="container py-3 flex-grow-1">
+					<Routes>
+						<Route path="/" element={<HomePage isLoggedIn={isLoggedIn} />} />
+						<Route
+							path="/login"
+							element={<LoginPage setIsLoggedIn={setIsLoggedIn} />}
+						/>
+						<Route
+							path="/chat"
+							element={
+								<ProtectedRoute isLoggedIn={isLoggedIn} redirectTo="/login">
+									<ChatPage />
+								</ProtectedRoute>
+							}
+						/>
+						<Route
+							path="/about"
+							element={<AboutPage isLoggedIn={isLoggedIn} />}
+						/>
+					</Routes>
+				</div>
+				<Footer />
+			</div>
+		</Router>
+	);
 }
 
 export default App;
